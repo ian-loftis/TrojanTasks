bc4e15ee7b4cbd3fd5222f4eebd94731a4a0178c
Ian Loftis <ianloftis@Ians-MacBook-Pro.local>
2017-11-12 13:53:17 -0800

    initial commit

---
+++ src/objects/TaskManager.java
@@ -0,0 +1,16 @@
+package objects;
+
+import java.util.ArrayList;
+
+public class TaskManager {
+
+    public void RandomAssignment(ArrayList<User> users, Task task) {
+
+    }
+
+    public void AddTaskToUser(User user, Task task) {
+
+    }
+}
+
+
